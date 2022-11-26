2a606cf06a5d327538939c88f1efc64c1b72407b
charlie-ward <59178341+Charlie-Ward@users.noreply.github.com>
2022-11-26 13:38:50 +0000

    Initial Commit

---
+++ src/main/java/info/charlieward/skyfallranks/SkyfallRanks.java
@@ -0,0 +1,17 @@
+package info.charlieward.skyfallranks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyfallRanks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
